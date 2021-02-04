b105b758d7b20cd2f339e64075e5e969294df505
Animesh Sutradhar <was.animesh@gmail.com>
2021-02-04 11:38:34 +0530

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
               ensure  =>  present,
               user    =>  'root',
               type    =>  'ssh-rsa',
-              key     =>  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmRgzrzp5PkjLmlMhOQCo6FecwNwo227uIM+U54iQBPWAswfC57PcHtPgvV3kFOUuSovzdbT8Q9zmy59/g7rV2VqsYoI7cD0f/ySdljFvD9qcXzNfB5FLlaJVCJ92PxZDJyD8uxAqC7nTVnxDlDpn2hAy7kCtD1pUvj4gfCuRInYeHILZCU8EkOysLheFbaBu+3ODpGTy/zjRYeDbVrCWortctihtjNcLlZ6jMqmY2lw480+8yspP+8DI8pAKOhzjVfxOr3fxfIR2DFn37QWA11EhRQeOWkWPbBEE10zSubtuykfBJZyVtsN6YonnGz+J5Nuz45aHbhP/vB+nynzMv root@ctos7srv01.asdcloud.com',
+              key     =>  'AAAAB3NzaC1yc2EAAAADAQABAAABAQDmRgzrzp5PkjLmlMhOQCo6FecwNwo227uIM+U54iQBPWAswfC57PcHtPgvV3kFOUuSovzdbT8Q9zmy59/g7rV2VqsYoI7cD0f/ySdljFvD9qcXzNfB5FLlaJVCJ92PxZDJyD8uxAqC7nTVnxDlDpn2hAy7kCtD1pUvj4gfCuRInYeHILZCU8EkOysLheFbaBu+3ODpGTy/zjRYeDbVrCWortctihtjNcLlZ6jMqmY2lw480+8yspP+8DI8pAKOhzjVfxOr3fxfIR2DFn37QWA11EhRQeOWkWPbBEE10zSubtuykfBJZyVtsN6YonnGz+J5Nuz45aHbhP/vB+nynzMv',
       }
 }
